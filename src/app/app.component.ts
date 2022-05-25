5b0df2093ce6f3a0cc49ee2519982a656236dd7d
Rigoberto Caionda <rigobertocaionda98@gmail.com>
2022-05-25 16:59:02 +0100

    Creating the project initial layouts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'devsfood-angular';
+  inputActive = false;
+
+  handleInputFocus() {
+    this.inputActive = true;
+  }
+
+  handleInputBlur() {
+    // So deverei colocar false caso o user nao tiver digitado nada, refazer essa parte.
+    this.inputActive = false;
+  }
 }
