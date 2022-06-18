a841c03f119d8c2fbeaf239b7b9432c86b79623a
Rigoberto Caionda <rigobertocaionda98@gmail.com>
2022-06-18 19:54:08 +0100

    Adding things

--- src/app/shared/layouts/modal/modal.component.ts
+++ src/app/shared/layouts/modal/modal.component.ts
@@ -1,33 +1,55 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  Input,
+  OnInit,
+  Output,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
 })
 export class ModalComponent implements OnInit {
   @Input() product: any;
   @Output() cancelBtnClicked = new EventEmitter();
+  @ViewChild('modal') modal!: ElementRef;
   qt = 1;
   cart: any[] = [];
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService) {}
 
-  ngOnInit(): void {
-    
-  }
+  ngOnInit(): void {}
 
   handleClick(type: string) {
     if (type == '-') {
-      if(this.qt > 1) {
+      if (this.qt > 1) {
         this.qt--;
       }
     }
-    
+
     if (type == '+') {
       this.qt++;
     }
   }
+
+  @HostListener('document:click', ['$event.target']) onClick(
+    targetElement: HTMLElement
+  ) {
+    if (!targetElement) {
+      return;
+    }
+    const modalIsClicked = this.modal.nativeElement.contains(targetElement);
+    if (!modalIsClicked) {
+      //this.cancelBtnClicked.emit('clicado'); // Nessa linha
+    }
+  }
+
   cancel() {
     this.cancelBtnClicked.emit('clicado');
     this.qt = 1;
@@ -35,19 +57,17 @@ export class ModalComponent implements OnInit {
 
   addCart() {
     this.productService.getCartValue().subscribe({
-      next: res => this.cart = res
+      next: (res) => (this.cart = res),
     });
     let cartProduct = { ...this.product, qt: this.qt };
     let sameProduct = this.cart.findIndex((item) => item.id == cartProduct.id);
-    console.log(sameProduct)
-    if(sameProduct > -1) {
+    if (sameProduct > -1) {
       this.cart[sameProduct].qt += cartProduct.qt;
     } else {
-      this.cart.push(cartProduct); 
+      this.cart.push(cartProduct);
     }
     this.productService.setCartValue(this.cart);
-    this.productService.getCartValue().subscribe({
-      next: res => console.log(res)
-    });
+    this.cancelBtnClicked.emit('clicado');
+    this.qt = 1;
   }
 }
