235494b8918084fb9c5f3ad7fe8d2eb7728146ec
Rigoberto Caionda <rigobertocaionda98@gmail.com>
2022-08-10 15:38:04 +0100

    Updating the project to multilingual project

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreatePproductFormComponent } from '../shared/layouts/create-pproduct-form/create-pproduct-form.component';
-import { CreateRoleComponent } from '../shared/layouts/create-role/create-role.component';
+import { CreateRoleComponent } from '../shared/layouts/create-category/create-role.component';
 import { EditProductFormComponent } from '../shared/layouts/edit-product-form/edit-product-form.component';
 import { SignupComponent } from '../views/signup/signup.component';
 import { AdminDashboardComponent } from '../views/admin-dashboard/admin-dashboard.component';
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,7 +7,7 @@ import { ErrorComponent } from './layouts/error/error.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { CreatePproductFormComponent } from './layouts/create-pproduct-form/create-pproduct-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { CreateRoleComponent } from './layouts/create-role/create-role.component';
+import { CreateRoleComponent } from './layouts/create-category/create-role.component';
 import { EditProductFormComponent } from './layouts/edit-product-form/edit-product-form.component';
 import { DeleteProductComponent } from './delete-product/delete-product.component';
 
